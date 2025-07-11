f590a4e3fe2751b8a88ebb6718d720a67821007d
Gino Laranjeiras <gino.laranjeiras@onikel.com>
2025-07-11 14:43:01 -0300

    feat(app_bar): add back button navigation to custom sliver app bar
    Add back button functionality when navigation is possible to improve user experience and provide consistent navigation pattern

--- lib/view/component/ui/app_bar/custom_sliver_app_bar.dart
+++ lib/view/component/ui/app_bar/custom_sliver_app_bar.dart
@@ -1,15 +1,24 @@
 import '../../../../utils/imports/common_libs.dart';
 
-
 class CustomSliverAppBar extends StatelessWidget {
   const CustomSliverAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final router = GoRouter.of(context);
+    final canPop = router.canPop();
+
     return SliverAppBar(
       title: Text(appConstants.appName),
       floating: true,
       snap: true,
+      leading: canPop
+          ? IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => router.pop(),
+              tooltip: 'Voltar',
+            )
+          : null,
       actions: [
         const LanguageSelector(),
         const ThemeToggleButton(),
