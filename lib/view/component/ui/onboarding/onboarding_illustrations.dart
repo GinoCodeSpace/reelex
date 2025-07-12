eb2330775a01574f579427f2226df7444159e357
Gino Laranjeiras <gino.laranjeiras@onikel.com>
2025-07-12 10:54:53 -0300

    feat(onboarding): add animations to onboarding illustrations
    Add flutter_animate dependency and implement various animations for onboarding illustrations to enhance user experience. Includes fade, scale, slide, rotate and shimmer effects with carefully timed sequences.

--- lib/view/component/ui/onboarding/onboarding_illustrations.dart
+++ lib/view/component/ui/onboarding/onboarding_illustrations.dart
@@ -1,10 +1,28 @@
 import '../../../../utils/imports/common_libs.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 /// Ilustrações SVG para as páginas de onboarding
 class OnboardingIllustrations {
-  static Widget get favorites => _FavoritesIllustration();
-  static Widget get chef => _ChefIllustration();
-  static Widget get delivery => _DeliveryIllustration();
+  static Widget get favorites => _FavoritesIllustration()
+      .animate()
+      .fadeIn(duration: 800.ms, curve: Curves.easeOutCubic)
+      .scale(begin: const Offset(0.8, 0.8), duration: 600.ms, curve: Curves.elasticOut)
+      .then(delay: 200.ms)
+      .shimmer(duration: 1500.ms, color: Colors.white.withValues(alpha: 0.3));
+  
+  static Widget get chef => _ChefIllustration()
+      .animate()
+      .fadeIn(duration: 800.ms, curve: Curves.easeOutCubic)
+      .slideY(begin: 0.3, duration: 700.ms, curve: Curves.easeOutBack)
+      .then(delay: 300.ms)
+      .shimmer(duration: 1500.ms, color: Colors.white.withValues(alpha: 0.3));
+  
+  static Widget get delivery => _DeliveryIllustration()
+      .animate()
+      .fadeIn(duration: 800.ms, curve: Curves.easeOutCubic)
+      .slideX(begin: 0.3, duration: 700.ms, curve: Curves.easeOutBack)
+      .then(delay: 400.ms)
+      .shimmer(duration: 1500.ms, color: Colors.white.withValues(alpha: 0.3));
 }
 
 class _FavoritesIllustration extends StatelessWidget {
@@ -59,7 +77,21 @@ class _FavoritesIllustration extends StatelessWidget {
             Icons.favorite,
             size: 80,
             color: primaryColor,
-          ),
+          )
+              .animate()
+              .scale(
+                begin: const Offset(1.0, 1.0),
+                end: const Offset(1.1, 1.1),
+                duration: 2000.ms,
+                curve: Curves.easeInOut,
+              )
+              .then()
+              .scale(
+                begin: const Offset(1.1, 1.1),
+                end: const Offset(1.0, 1.0),
+                duration: 2000.ms,
+                curve: Curves.easeInOut,
+              ),
           // Ícones menores ao redor
           Positioned(
             top: 60,
@@ -68,7 +100,16 @@ class _FavoritesIllustration extends StatelessWidget {
               Icons.restaurant,
               size: 32,
               color: secondaryColor,
-            ),
+            )
+                .animate(delay: 500.ms)
+                .fadeIn(duration: 600.ms)
+                .slideX(begin: 0.5, duration: 400.ms, curve: Curves.easeOut)
+                .then(delay: 1000.ms)
+                .rotate(begin: 0, end: 0.1, duration: 1500.ms)
+                .then()
+                .rotate(begin: 0.1, end: -0.1, duration: 3000.ms)
+                .then()
+                .rotate(begin: -0.1, end: 0, duration: 1500.ms),
           ),
           Positioned(
             bottom: 60,
@@ -77,7 +118,24 @@ class _FavoritesIllustration extends StatelessWidget {
               Icons.local_pizza,
               size: 28,
               color: primaryColor.withValues(alpha: 0.7),
-            ),
+            )
+                .animate(delay: 700.ms)
+                .fadeIn(duration: 600.ms)
+                .slideY(begin: 0.5, duration: 400.ms, curve: Curves.bounceOut)
+                .then(delay: 800.ms)
+                .scale(
+                  begin: const Offset(1.0, 1.0),
+                  end: const Offset(1.2, 1.2),
+                  duration: 1800.ms,
+                  curve: Curves.easeInOut,
+                )
+                .then()
+                .scale(
+                  begin: const Offset(1.2, 1.2),
+                  end: const Offset(1.0, 1.0),
+                  duration: 1800.ms,
+                  curve: Curves.easeInOut,
+                ),
           ),
           Positioned(
             top: 80,
@@ -86,7 +144,12 @@ class _FavoritesIllustration extends StatelessWidget {
               Icons.cake,
               size: 24,
               color: secondaryColor.withValues(alpha: 0.8),
-            ),
+            )
+                .animate(delay: 900.ms)
+                .fadeIn(duration: 600.ms)
+                .scale(begin: const Offset(0.5, 0.5), duration: 500.ms, curve: Curves.elasticOut)
+                .then(delay: 1200.ms)
+                .rotate(begin: 0, end: 1, duration: 4000.ms),
           ),
         ],
       ),
@@ -146,7 +209,21 @@ class _ChefIllustration extends StatelessWidget {
             Icons.person_outline,
             size: 80,
             color: primaryColor,
-          ),
+          )
+              .animate()
+              .slideY(
+                begin: 0,
+                end: -0.02,
+                duration: 2500.ms,
+                curve: Curves.easeInOut,
+              )
+              .then()
+              .slideY(
+                begin: -0.02,
+                end: 0,
+                duration: 2500.ms,
+                curve: Curves.easeInOut,
+              ),
           // Chapéu do chef
           Positioned(
             top: 80,
@@ -154,7 +231,16 @@ class _ChefIllustration extends StatelessWidget {
               Icons.restaurant_menu,
               size: 40,
               color: secondaryColor,
-            ),
+            )
+                .animate(delay: 400.ms)
+                .fadeIn(duration: 800.ms)
+                .slideY(begin: -0.3, duration: 600.ms, curve: Curves.bounceOut)
+                .then(delay: 1000.ms)
+                .rotate(begin: 0, end: 0.05, duration: 1800.ms)
+                .then()
+                .rotate(begin: 0.05, end: -0.05, duration: 3600.ms)
+                .then()
+                .rotate(begin: -0.05, end: 0, duration: 1800.ms),
           ),
           // Utensílios ao redor
           Positioned(
@@ -164,7 +250,14 @@ class _ChefIllustration extends StatelessWidget {
               Icons.kitchen,
               size: 28,
               color: primaryColor.withValues(alpha: 0.7),
-            ),
+            )
+                .animate(delay: 600.ms)
+                .fadeIn(duration: 600.ms)
+                .scale(begin: const Offset(0.3, 0.3), duration: 500.ms, curve: Curves.elasticOut)
+                .then(delay: 800.ms)
+                .shimmer(duration: 3000.ms, color: primaryColor.withValues(alpha: 0.4))
+                .then(delay: 1000.ms)
+                .shimmer(duration: 3000.ms, color: primaryColor.withValues(alpha: 0.4)),
           ),
           Positioned(
             bottom: 70,
@@ -173,7 +266,24 @@ class _ChefIllustration extends StatelessWidget {
               Icons.dining,
               size: 24,
               color: secondaryColor.withValues(alpha: 0.8),
-            ),
+            )
+                .animate(delay: 800.ms)
+                .fadeIn(duration: 600.ms)
+                .slideX(begin: -0.5, duration: 400.ms, curve: Curves.easeOut)
+                .then(delay: 1500.ms)
+                .scale(
+                  begin: const Offset(1.0, 1.0),
+                  end: const Offset(1.15, 1.15),
+                  duration: 2200.ms,
+                  curve: Curves.easeInOut,
+                )
+                .then()
+                .scale(
+                  begin: const Offset(1.15, 1.15),
+                  end: const Offset(1.0, 1.0),
+                  duration: 2200.ms,
+                  curve: Curves.easeInOut,
+                ),
           ),
         ],
       ),
@@ -233,7 +343,28 @@ class _DeliveryIllustration extends StatelessWidget {
             Icons.delivery_dining,
             size: 80,
             color: primaryColor,
-          ),
+          )
+              .animate()
+              .slideX(
+                begin: 0,
+                end: 0.03,
+                duration: 2000.ms,
+                curve: Curves.easeInOut,
+              )
+              .then()
+              .slideX(
+                begin: 0.03,
+                end: -0.03,
+                duration: 4000.ms,
+                curve: Curves.easeInOut,
+              )
+              .then()
+              .slideX(
+                begin: -0.03,
+                end: 0,
+                duration: 2000.ms,
+                curve: Curves.easeInOut,
+              ),
           // Elementos de entrega ao redor
           Positioned(
             top: 60,
@@ -242,7 +373,24 @@ class _DeliveryIllustration extends StatelessWidget {
               Icons.local_shipping,
               size: 32,
               color: secondaryColor,
-            ),
+            )
+                .animate(delay: 500.ms)
+                .fadeIn(duration: 600.ms)
+                .slideX(begin: 0.8, duration: 500.ms, curve: Curves.easeOutBack)
+                .then(delay: 1200.ms)
+                .slideX(
+                  begin: 0,
+                  end: 0.1,
+                  duration: 1500.ms,
+                  curve: Curves.easeInOut,
+                )
+                .then()
+                .slideX(
+                  begin: 0.1,
+                  end: 0,
+                  duration: 1500.ms,
+                  curve: Curves.easeInOut,
+                ),
           ),
           Positioned(
             bottom: 60,
@@ -251,7 +399,12 @@ class _DeliveryIllustration extends StatelessWidget {
               Icons.schedule,
               size: 28,
               color: primaryColor.withValues(alpha: 0.7),
-            ),
+            )
+                .animate(delay: 700.ms)
+                .fadeIn(duration: 600.ms)
+                .scale(begin: const Offset(0.2, 0.2), duration: 600.ms, curve: Curves.elasticOut)
+                .then(delay: 1000.ms)
+                .rotate(begin: 0, end: 1, duration: 8000.ms),
           ),
           Positioned(
             top: 80,
@@ -260,7 +413,25 @@ class _DeliveryIllustration extends StatelessWidget {
               Icons.location_on,
               size: 24,
               color: secondaryColor.withValues(alpha: 0.8),
-            ),
+            )
+                .animate(delay: 900.ms)
+                .fadeIn(duration: 600.ms)
+                .slideY(begin: -0.5, duration: 400.ms, curve: Curves.bounceOut)
+                .then(delay: 800.ms)
+                .scale(
+                  begin: const Offset(1.0, 1.0),
+                  end: const Offset(1.3, 1.3),
+                  duration: 1000.ms,
+                  curve: Curves.easeInOut,
+                )
+                .then()
+                .scale(
+                  begin: const Offset(1.3, 1.3),
+                  end: const Offset(1.0, 1.0),
+                  duration: 1000.ms,
+                  curve: Curves.easeInOut,
+                )
+                .then(delay: 2000.ms),
           ),
           // Indicador de "grátis"
           Positioned(
@@ -287,7 +458,14 @@ class _DeliveryIllustration extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
+            )
+                .animate(delay: 1100.ms)
+                .fadeIn(duration: 800.ms)
+                .scale(begin: const Offset(0.5, 0.5), duration: 600.ms, curve: Curves.elasticOut)
+                .then(delay: 2000.ms)
+                .shimmer(duration: 2500.ms, color: Colors.white.withValues(alpha: 0.6))
+                .then(delay: 1500.ms)
+                .shimmer(duration: 2500.ms, color: Colors.white.withValues(alpha: 0.6)),
           ),
         ],
       ),
